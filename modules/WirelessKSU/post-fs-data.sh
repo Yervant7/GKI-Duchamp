3711b114ab339092ad5c5ba6afd53aa769234ba1
Ahmed Al-Nassif <mr.ahmed.nassif@gmail.com>
2026-08-04 00:27:26 +0300

    refactor: add R8188EU staging driver as lkm
    Signed-off-by: Ahmed Al-Nassif <mr.ahmed.nassif@gmail.com>

--- modules/WirelessKSU/post-fs-data.sh
+++ modules/WirelessKSU/post-fs-data.sh
@@ -21,5 +21,7 @@ for mod in rtw88_8812a rtw88_8812au rtw88_8814a rtw88_8814au \
     ksud insmod $MODPATH/lkm/$mod.ko 2>/dev/null
 done
 
+ksud insmod $MODPATH/lkm/r8188eu.ko 2>/dev/null
+
 echo "rtw88 modules loaded" > /dev/kmsg
 lsmod | grep -E "rtw|mac80211|cfg80211" > /dev/kmsg 2>&1
